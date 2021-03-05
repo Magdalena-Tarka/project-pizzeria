ae8524603ba317078ba2244ca18aec48593b13b3
Magdalena Tarka <mxtarka@gmail.com>
2021-03-06 00:12:22 +0100

    Add subpages functionality

--- src/js/app.js
+++ src/js/app.js
@@ -4,10 +4,63 @@ import Cart from './components/Cart.js';
 
 const app = {
     
+  initPages: function() {
+    const thisApp = this;
+    
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;  // Dzięki temu w tej właściwości znajdą sie wszystkie dzieci contenera stron
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+      
+    const idFromHash = window.location.hash.replace('#/', '');
+      
+    let pageMatchingHash = thisApp.pages[0].id;
+      
+    for(let page of thisApp.pages) {
+      if(page.id == idFromHash) {
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+    //console.log('pageMatchingHash:', pageMatchingHash);
+    thisApp.activatePage(pageMatchingHash);
+      
+    for(let link of thisApp.navLinks) {
+      link.addEventListener('click', function(event) {
+        const clickedElement = this;
+        event.preventDefault();
+        
+        /* get page id from href attribute */
+        const id = clickedElement.getAttribute('href').replace('#', '');
+          
+        /* run thisApp.activatePage with that id */
+        thisApp.activatePage(id);
+          
+        /* change URL hash */
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
+    
+  activatePage: function(pageId) {
+    const thisApp = this;
+      
+    /* add class "active" to matching pages,remove from non-matching */
+      
+    for(let page of thisApp.pages) {   // W .toggle za pomoca drugiego argumentu możemy kontrolować to czy klasa zostanie nadana, czy nie!! Możemy tu użyć warunku takiego jak w bloku if!!
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+      
+    /* add class "active" to matching links,remove from non-matching */
+    for(let link of thisApp.navLinks) {
+      link.classList.toggle(  // Możemy to rozbić na taki zapis
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  },
+    
   initMenu: function() {
     const thisApp = this;
       
-    //console.log('thisApp.data:', thisApp.data);
     for(let productData in thisApp.data.products) {
       new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
       //console.log('XX. thisApp.data.products[productData].id, thisApp.data.products[productData]:', thisApp.data.products[productData].id, thisApp.data.products[productData]);
@@ -55,7 +108,8 @@ const app = {
     console.log('classNames:', classNames);
     console.log('settings:', settings);
     console.log('templates:', templates);
-      
+    
+    thisApp.initPages();
     thisApp.initData();
     //thisApp.initMenu();
     thisApp.initCart();
