7a1c76ffd39c0e90f5af1482524d96bb3e05079b
Magdalena Tarka <mxtarka@gmail.com>
2021-03-12 01:28:54 +0100

    Add method parseData, makeBooked, updateDOM

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,5 +1,5 @@
 import utils from '../utils.js';
-import {select, settings, templates} from '../settings.js';
+import {classNames, select, settings, templates} from '../settings.js';
 import AmountWidget from './AmountWidget.js';
 import DatePicker from './DatePicker.js';
 import HourPicker from './HourPicker.js';
@@ -24,24 +24,24 @@ class Booking {
         startDateParam,
         endDateParam,
       ],
-      eventCurrent: [
+      eventsCurrent: [
         settings.db.notRepeatParam,
         startDateParam,
         endDateParam,
       ],
-      eventRepeat: [
+      eventsRepeat: [
         settings.db.repeatParam,
         endDateParam,
       ],
     };
-    console.log('getData params', params);
+    //console.log('getData params', params);
 
     const urls = {
-      booking:      settings.db.url + '/' + settings.db.booking + '?' + params.booking.join('&'),   // zawiera adres endpointu API, który zwróci nam listę rezerwacji
-      eventsCurrent: settings.db.url + '/' + settings.db.booking + '?' + params.eventCurrent.join('&'),  // zwróci listę wydarzeń jednorazowych
-      eventsRepeat:  settings.db.url + '/' + settings.db.booking + '?' + params.eventRepeat.join('&'),  // zwróci liste wydarzeń cyklicznych
+      booking: settings.db.url + '/' + settings.db.booking + '?' + params.booking.join('&'),   // zawiera adres endpointu API, który zwróci nam listę rezerwacji
+      eventsCurrent: settings.db.url + '/' + settings.db.event + '?' + params.eventsCurrent.join('&'),  // zwróci listę wydarzeń jednorazowych
+      eventsRepeat: settings.db.url + '/' + settings.db.event + '?' + params.eventsRepeat.join('&'),  // zwróci liste wydarzeń cyklicznych
     };
-    console.log('urls', urls);
+    //console.log('urls', urls);
 
     Promise.all([   /// Pobieramy z API listę rezerwacji
       fetch(urls.booking),
@@ -59,10 +59,93 @@ class Booking {
         ]);
       })
       .then(function([bookings, eventsCurrent, eventsRepeat]) {
-        console.log('bookings:', bookings);
-        console.log('eventsCurrent:', eventsCurrent);
-        console.log('eventsRepeat:', eventsRepeat);
+        //console.log('bookings:', bookings);
+        //console.log('eventsCurrent:', eventsCurrent);
+        //console.log('eventsRepeat:', eventsRepeat);
+        thisBooking.parseData(bookings, eventsCurrent, eventsRepeat);
       });
+  }
+
+  parseData(bookings, eventsCurrent, eventsRepeat) {
+    const thisBooking = this;
+
+    thisBooking.booked = {};
+
+    for(let item of bookings) {
+      thisBooking.makeBooked(item.date, item.hour, item.duration, item.table);
+    }
+
+    for(let item of eventsCurrent) {
+      thisBooking.makeBooked(item.date, item.hour, item.duration, item.table);
+    }
+
+    const minDate = thisBooking.datePicker.minDate;
+    const maxDate = thisBooking.datePicker.maxDate;
+
+    for(let item of eventsRepeat) {
+      if(item.repeat == 'daily') {
+        // for(let loopDate = minDate; loopDate <= maxDate; utils.addDays(loopDate, 1)) {  // Jak zostawimy w ten sposób to przeglądarka się zwiesi, bp to niekończąca się pętl, bo ostatniego wyrażenia nigdzie nie zapisujemy
+        for(let loopDate = minDate; loopDate <= maxDate; loopDate = utils.addDays(loopDate, 1)) {  //Kolejna pętla, bo tu są wydarzenia cykliczne, więc nie tylko data jednego dnia, czyli iterujemy po jakimś zakresie dat od min do max date. Na dacie nie możemy zrobić ++ wieć posłuzy do tego funkcja addDays(obiekt daty, liczba dni)
+          thisBooking.makeBooked(utils.dateToStr(loopDate), item.hour, item.duration, item.table);  // Korzystamy z dateToStr(), zeby skonwertowac datę na tekst w odpowiednim formacie
+        }
+      }
+    }
+    console.log('thisBooking.booked:', thisBooking.booked);
+
+    thisBooking.updateDOM();
+  }
+
+  makeBooked(date, hour, duration, table) {
+    const thisBooking = this;
+
+    if(typeof thisBooking.booked[date] == 'undefined') {   // Sprawdzamy, czy mamy już jakiś wpis dla tej daty
+      thisBooking.booked[date] = {};   // Jeśli nie to chcemy stworzyć pusty obiekt
+    }
+
+    const startHour = utils.hourToNumber(hour);   // Konwersja godziny do liczby (12:30 do 12,5)
+    
+    for(let hourBlock = startHour; hourBlock < startHour + duration; hourBlock += 0.5) {   // hourBlock to 0.5 godzinny blok czasowy. Same as for(index=0; index<3; index++){} //w konsoli będzie index=0, index=1, index=2
+      //console.log('loop:', hourBlock);
+
+      if(typeof thisBooking.booked[date][hourBlock] == 'undefined') {   // Wykonujemu podobne sprawdzenie jak wczesniej dla daty
+        thisBooking.booked[date][hourBlock] = [];
+      }
+      thisBooking.booked[date][hourBlock].push(table);   // Dodajemy tutaj numery stolików do naszego obiektu z datami i godzinami
+    }
+  }
+
+  updateDOM() {
+    const thisBooking = this;
+
+    thisBooking.date = thisBooking.datePicker.value;   // Razem z kodem poniżej są to wartości wybrane aktualnie przez użytkownika
+    thisBooking.hour = utils.hourToNumber(thisBooking.hourPicker.value);
+
+    let allAvailabele = false;   // Ta zmienna oznacza, że tego dnia wszystkie stoliki są dostępne, narazie ma false
+
+    if(   // Jeśli okaże się, że w obiekcie thisBooking.booked dla tej daty nie ma obiektu lub dla tej daty i godziny nie istnieje tablica będzie to oznaczało, że żaden stolik nie jest zajęty, czyli wszystkie stoliki są dostępne, wtedy zmieny wartość allAvailabele na true
+      typeof thisBooking.booked[thisBooking.date] == 'undefined'
+      ||
+      typeof thisBooking.booked[thisBooking.date][thisBooking.hour] == 'undefined'
+    ) {
+      allAvailabele = true;
+    }
+
+    // uruchamiamy teraz pętlę, kt będzie iterować przez wszystkie stoliki widoczne na mapie na str booking. Pobieramy id aktulnego stolika, zas sprawdzamy czy nr stolika jest liczbą, zaś 
+    for(let table of thisBooking.dom.tables) {
+      let tableId = table.getAttribute(settings.booking.tableIdAttribute);
+      if(!isNaN(tableId)) {
+        tableId = parseInt(tableId);
+      }
+
+      if(!allAvailabele   // Sprawdzamy czy nie wszystkie stoliki są dostępne, czyli czy któryś stolik jest zajęty, dalej sprawdzamy czy któryś stolik jest zajęty o tym id
+        &&
+        thisBooking.booked[thisBooking.date][thisBooking.hour].includes(tableId)
+      ) {
+        table.classList.add(classNames.booking.tableBooked);
+      } else {
+        table.classList.remove(classNames.booking.tableBooked);
+      }
+    }
   }
 
   render(element) {
@@ -73,10 +156,14 @@ class Booking {
     thisBooking.dom = {};
     thisBooking.dom.wrapper = element;
     thisBooking.dom.wrapper.innerHTML = generatedHTML;
+
     thisBooking.dom.peopleAmount = element.querySelector(select.booking.peopleAmount);
     thisBooking.dom.hoursAmount = element.querySelector(select.booking.hoursAmount);
+
     thisBooking.dom.datePicker = element.querySelector(select.widgets.datePicker.wrapper);
     thisBooking.dom.hourPicker = element.querySelector(select.widgets.hourPicker.wrapper);
+
+    thisBooking.dom.tables = element.querySelectorAll(select.booking.tables);
   }
 
   initWidgets() {
@@ -93,6 +180,10 @@ class Booking {
     });
     thisBooking.dom.hoursAmount.addEventListener('click', function(event) {
       event.preventDefault();
+    });
+
+    thisBooking.dom.wrapper.addEventListener('update', function() {
+      thisBooking.updateDOM();
     });
   }
 }
